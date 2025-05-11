8865bcecee5e889c6495f03e4ba299712f519c6c
Branchez <brian.sanchez@homegauge.com>
2025-05-11 00:53:40 -0600

    devise project model migration

---
+++ prisma/migrations/20250511065145_project_model_migration/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    CONSTRAINT "Project_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
